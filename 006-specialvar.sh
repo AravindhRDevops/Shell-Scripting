334091b39ba86586fdf7a674bc1670469c59a8d2
Ramachandran <Aravindh.Ramachandran@emc.com>
2024-03-15 04:04:02 +0530

    Bash Autopush

--- 006-specialvar.sh
+++ 006-specialvar.sh
@@ -6,4 +6,7 @@ echo -e "value of a is $a"
 
 echo -e "name of the script is $0"
 echo -e "name of the script is $$"
-echo -e "name of the scipt is $*"
+echo -e "name of the scipt is $**"
+echo -e "name of the scipt is $#"
+echo -e "name of the scipt is $@"
+echo -e "name of the scipt is $9"
