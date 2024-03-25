211bc6d96237c4668f32b688254433ce195faca8
Ramachandran <Aravindh.Ramachandran@emc.com>
2024-03-25 13:50:19 +0530

    Bash Auto Push

---
+++ 007-function.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "hello world"
+f
+
+f
+
+f()
+{today is Monday
+tomorrow is thuesday
+
+}
+
+f 
+echo "what we are learining today will long last"
+echo "i am trying my level best"
+
+f
