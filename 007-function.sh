8cd1151191e4a383ce7f15ec967da91ac1bc1d43
Ramachandran <Aravindh.Ramachandran@emc.com>
2024-03-25 14:09:54 +0530

    Bash Auto Push

--- 007-function.sh
+++ 007-function.sh
@@ -10,3 +10,13 @@ f
 echo "what we are learining today will long last"
 echo "i am trying my level best"
 f
+
+f 
+echo "i am good"
+echo "travel to st joes"
+f
+
+f 
+echo "good time remains"
+echo "gonna blast dreams"
+f
