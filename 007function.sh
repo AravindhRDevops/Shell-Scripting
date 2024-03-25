c4d1afb23ee87f5123ad16537725d2d2d2facdde
Ramachandran <Aravindh.Ramachandran@emc.com>
2024-03-26 00:07:50 +0530

    Bash Auto Push

---
+++ 007function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "hello world"
+f
+
+f
+
+f()
+{today is Monday
+tomorrow is thuesday
+
+}
+
+f 
+echo "what we are learining today will long last"
+echo "i am trying my level best"
+
+f
+
+echo "Number of opened session $(who|wc -l)"
